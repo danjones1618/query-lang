9de0e8a76649af48505bc3e8316dea7073077d98
Dan Jones <hello@danjones.dev>
2025-03-02 17:49:02 +0100

    Feat: convert to Django Q objects

--- python/query_lang/_core.pyi
+++ python/query_lang/_core.pyi
@@ -1,6 +1,11 @@
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from django.db.models.query import Q
+
 __all__ = [
     "ParsingError",
-    "parse_to_string",
+    "parse_to_django_q",
 ]
 
 class ParsingError(Exception):
@@ -8,4 +13,4 @@ class ParsingError(Exception):
 
     def __init__(self, message: str) -> None: ...
 
-def parse_to_string(to_parse: str) -> str: ...
+def parse_to_django_q(to_parse: str) -> "Q": ...
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ fn main() {
     let to_parse = r#"(name     = "hi ehllo * there _ yaes" or blah =~ "7*") and location = "eu-1" and (yes != "okay" or yes = "hmm nice")"#;
     match parse_query_string(&to_parse) {
         Ok(e) => println!("{e:?}"),
-        Err(e) => println!("{e}"),
+        Err(e) => println!("{e:?}"),
     }
 }
