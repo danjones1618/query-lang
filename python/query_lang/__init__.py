6568ba7178f58ac604e4a8680836a7c0622ed61f
Dan Jones <hello@danjones.dev>
2025-03-02 20:16:31 +0100

    Feat: organise into mono-repo for multiple benchmarks to compare
    Goal is to implement different parsers for same language to compare.

--- python/query_lang/__init__.py
+++
@@ -1,6 +0,0 @@
-from query_lang._core import ParsingError, parse_to_django_q
-
-__all__ = [
-    "ParsingError",
-    "parse_to_django_q",
-]
--- test.py
+++ test.py
@@ -1,7 +1,8 @@
-import query_lang as c
+import rust_pest as c
 from rich import print
 
-try:
-    print(c.parse_to_string('hi = "yes" and locatio(n = "UK"'))
-except c.ParsingError as e:
-    print(e)
+print(
+    c.parse_to_django_q(
+        '(hi = "yes" and location != "UK") or (yes !~ "okay" or no = "wow") or (a = "1" and b =    "3" and c         = "4") or a = "1"'
+    )
+)
